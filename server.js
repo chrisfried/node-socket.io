9115fbf7acfc081fe0f15009228caa70812c5054
Chris Fried <fried.chris@gmail.com>
2018-07-03 08:25:02 -0500

    Join and leave rooms.

--- server.js
+++ server.js
@@ -8,14 +8,31 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .use((req, res) => res.sendFile(INDEX))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+io.on('connection', socket => {
+  socket.join('lobby', () => {});
+
+  socket.on('register', id => {
+    socket.bungieId = id;
+  });
+
+  socket.on('joinRoom', room => {
+    socket.join(room, () => {
+      socket.leave('lobby');
+    });
+  });
+
+  socket.on('leaveRoom', room => {
+    socket.leave(room, () => {
+      socket.join('lobby');
+    });
+  });
+
+  socket.on('disconnect', () => {});
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
